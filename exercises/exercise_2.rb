1b1b50bddadbdadd347279a8c9b8bf80608a2cf1
Catherine Hu <catherinehu_9898@hotmail.com>
2021-08-17 21:46:42 +0000

    exercise 2 done: find by store id and change name of store1"

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+puts "Store 1: #{@store1.name}"
+@store2 = Store.find_by(id: 2)
+puts "Store 2: #{@store2.name}"
+
+@store1.name = "Not Burnaby"
+puts "Store 1 name now: #{@store1.name}"
